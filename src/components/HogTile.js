a75ce11f3673f1d10bbc421d971011db09d2eb49
Paul-Cl-ark <clark.paul123@gmail.com>
2019-01-11 15:12:43 +0000

    Done

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -51,12 +51,3 @@ export default class HogTile extends Component {
     )
   }
 }
-
-//
-// {
-//   name: 'Mudblood',
-//   specialty: 'Mediocre magic',
-//   greased: false,
-//   'weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water': 2.0,
-//   'highest medal achieved': 'bronze'
-// }
